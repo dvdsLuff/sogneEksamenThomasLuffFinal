227473962240f5bb9b018d0966dec0f8bb76c52f
dvdsLuff <thomasluff@hotmail.com>
2021-06-11 00:22:55 +0200

    comit

---
+++ sogneEksamen/src/test/java/com/example/sogneeksamen/SogneEksamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sogneeksamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SogneEksamenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
